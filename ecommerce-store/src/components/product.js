683b725e03beec37c5ad445faedffaf34391f450
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-08-15 15:32:50 -0500

    added logic to map id's & skus

--- ecommerce-store/src/components/product.js
+++ ecommerce-store/src/components/product.js
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 
 const Product = ({skus, product}) => {
   const stripe = window.Stripe("pk_test_Q0pwIJG2dqeCmpkhSxyEXmJs00fkLGcSYc")
-  const [sku, setSku] = useState("sku_FcfKdpbjBAVOZz")
+  const [sku, setSku] = useState(skus[0].node.id)
 
   const placeOrder = () => {
   stripe.redirectToCheckout({
@@ -22,9 +22,11 @@ const Product = ({skus, product}) => {
         <img src="https://picsum.photos/200/300" alt=""/>
         <h3>{product.name}</h3>
         <select value={sku} onChange={e => setSku(e.target.value)}>
-          <option value="sku_FcfKdpbjBAVOZz">Small</option>
-          <option value="sku_FcMf94GZ1kfqg4">Medium</option>
-          <option value="sku_FcfLTZYfOxIAuu">Large</option>
+          {skus.map(edge => (
+          <option key={edge.node.id} value={edge.node.id}>
+            {edge.node.attributes.name}
+            </option>
+          ))}
         </select>
         <button onClick={placeOrder}>Buy Me</button>
       </article>
